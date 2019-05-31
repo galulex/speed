7654e01b60b0883345caa8a3ff2eac0128360576
galulex <galulex@gmail.com>
2019-05-31 15:27:58 +0300

    Update manifest

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
-// var staticCacheName = 'static';
-// var version = 3;
+var staticCacheName = 'static';
+var version = 3;
 
-// function updateCache() {
-//   return caches.open(staticCacheName + version).then(function (cache) {
-//     return cache.addAll(['/offline.html']);
-//   });
-// };
+function updateCache() {
+  return caches.open(staticCacheName + version).then(function (cache) {
+    return cache.addAll(['/offline.html']);
+  });
+};
 
-// self.addEventListener('install', function (event) {
-//     event.waitUntil(updateCache());
-// });
+self.addEventListener('install', function (event) {
+    event.waitUntil(updateCache());
+});
 
-// var doesRequestAcceptHtml = function(request) {
-//   return request.headers.get('Accept').split(',').some(function (type) { return type === 'text/html'; });
-// };
+var doesRequestAcceptHtml = function(request) {
+  return request.headers.get('Accept').split(',').some(function (type) { return type === 'text/html'; });
+};
 
-// self.addEventListener('fetch', function (event) {
-//   var request = event.request;
-//   if (doesRequestAcceptHtml(request)) {
-//     event.respondWith(fetch(request).catch(function () {
-//       return caches.match('/offline.html');
-//     }));
-//   } else {
-//     event.respondWith(caches.match(request).then(function (response) {
-//       return response || fetch(request);
-//     }));
-//   }
-// });
+self.addEventListener('fetch', function (event) {
+  var request = event.request;
+  if (doesRequestAcceptHtml(request)) {
+    event.respondWith(fetch(request).catch(function () {
+      return caches.match('/offline.html');
+    }));
+  } else {
+    event.respondWith(caches.match(request).then(function (response) {
+      return response || fetch(request);
+    }));
+  }
+});
